6a5aa5ca6cc227720dae1ea83b9b92cfb4ee7d08
Diego Ramon Bezerra Andrade <160649624+Diego55654@users.noreply.github.com>
2026-09-13 23:48:13 -0300

    add:first_backup_script_version
    here only stores the date from operation (backup).

--- script/script_backup.sh
+++ script/script_backup.sh
@@ -1,15 +1,35 @@
 #!bin/bash
 
 main(){
+    
+    cur_dir="$PWD"
+    dir_test="$HOME/backups_test_opl" #by SSH update connection?
+    backup_date="$(date +%Y-%m-%d_%H-%M-%S)"
+    log_file="$dir_test/backup_log_opl.txt" 
 
-	local cur_dir= $(pwd)
-	local dir_dest="/home/user/docs"
-    local backup_date = "$(date +%Y-%m-%d_%H-%M-%S)"
-    local log_file = "$dir_dest/backup_log_opl.txt" 
+    echo '=========================================================================' >> "$log_file"
+    
+    #Confirm if all the things's right
+    read -p "Let's start copying $cur_dir to $dir_test, alright? (Y/n) " response 
+ 
+    #log file -> date here
+    echo "$backup_date - Backup Finished" >> "$log_file"
+    
+    #Only accepts the 'Y'string and not empty entry 
+    if [ "$response" != "Y" ] || [ -z "$response" ]; then
+        echo 'Stop, go back and press 'Y''
+    else
+        echo 'copying...'
+         
+        : 'Copy recursively, it because Folders like: 
+        /DVD,/CD,ART,/VMC...'
 
-	echo '$cur_dir'
-	echo '$destinatio_dir'
+        cp -r "$cur_dir"/* "$dir_test/"
+
+        ls -la "$dir_test"
+    fi
 }
+
 main
 
 
