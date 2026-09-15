20a5454c254372dde57d7af7143358fdd8b22ffb
Diego Ramon Bezerra Andrade <160649624+Diego55654@users.noreply.github.com>
2026-09-15 13:25:01 -0300

    fix/testing: put >&2 (apt/echo)
    Transform messages/errors from sysout(1) to stderr(2). It's avoid pollute the variables itself.

--- script/helper.sh
+++ script/helper.sh
@@ -5,24 +5,24 @@ ok_zenity() {
         return 0
     fi
 
-    echo "[-] The 'zenity' tool(GUI) was not found."
+    echo "[-] The 'zenity' tool(GUI) was not found." >&2
     read -rp "[?] Would you like install it now to continue by Graphic Interface?(Y/n) " resp
 
     if [[ "$resp" =~ ^[Yy]$ ]] || [ -z "$resp" ]; then
         echo "[+] Installing zenity..."
         
         if command -v apt >/dev/null 2>&1; then
-            sudo apt update && sudo apt install -y zenity
+            sudo apt update && sudo apt install -y zenity >&2
         elif command -v dnf >/dev/null 2>&1; then
-            sudo dnf install -y zenity
+            sudo dnf install -y zenity >&2
         elif command -v pacman >/dev/null 2>&1; then
-            sudo pacman -S --noconfirm zenity
+            sudo pacman -S --noconfirm zenity >&2
         else
-            echo "[-] Package Manager don't support it. Download 'zenity' manually."
+            echo "[-] Package Manager don't support it. Download 'zenity' manually." >&2
             return 1
         fi
     else
-        echo "[!] Installation stopped by user."
+        echo "[!] Installation stopped by user." >&2
         return 1
     fi
 }
